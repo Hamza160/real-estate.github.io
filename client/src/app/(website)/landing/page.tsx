9d4320b681c1307e1800cd7e29958cd05bc19487
= <=>
2025-04-26 11:05:10 +0500

    feat: Header, Features, Discover Section and Call to action section Design Completed

--- client/src/app/(website)/landing/page.tsx
+++ client/src/app/(website)/landing/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
 
 export default function LandingPage() {
     return (
         <div>
             <HeroSection />
+            <FeaturesSection />
+            <DiscoverSection />
+            <CallToActionSection />
         </div>
     )
 }
